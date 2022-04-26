e3940348e70e7144b03b1e4a7d7e87eeeec5828f
hkj <hkj1984@gmail.com>
2022-04-26 22:49:27 +0900

    유효성 검사 추가

--- src/main/java/tacos/Taco.java
+++ src/main/java/tacos/Taco.java
@@ -4,11 +4,18 @@ import java.util.List;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 /**
  * 타코 디자인
  */
 @Data
 public class Taco {
+	@NotNull
+	@Size(min = 5, message = "Name must be at least 5 characters long")
 	private String name;
+
+	@Size(min = 1, message = "You meus choose at least 1 ingredient")
 	private List<String> ingredients;
 }
--- src/main/java/tacos/web/DesignTacoController.java
+++ src/main/java/tacos/web/DesignTacoController.java
@@ -2,6 +2,7 @@ package tacos.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +11,7 @@ import lombok.extern.slf4j.Slf4j;
 import tacos.Ingredient;
 import tacos.Taco;
 
+import javax.validation.Valid;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -58,7 +60,10 @@ public class DesignTacoController {
 	}
 
 	@PostMapping
-	public String processDesign(Taco design) {
+	public String processDesign(@Valid Taco design, Errors errors) {
+		if(errors.hasErrors()) {
+			return "design";
+		}
 
 		// TODO : 선택된 식자재 내역을 저장한다.
 		log.info("Processing design:" + design);
