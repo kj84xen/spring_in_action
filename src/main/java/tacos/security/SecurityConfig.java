6d634fa651b9bdaa3d548517bf0525016ed0ec18
hkj <hkj1984@gmail.com>
2022-05-01 00:13:47 +0900

    JDBC 기반 사용자 스토어로 변경

--- src/main/java/tacos/security/SecurityConfig.java
+++ src/main/java/tacos/security/SecurityConfig.java
@@ -1,32 +1,39 @@
 package tacos.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    DataSource dataSource;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests()
                 .antMatchers("/design", "/orders")
                 .hasAuthority("ROLE_USER")
-                .antMatchers("/", "/**")
+                .antMatchers("/", "/**", "/h2-console/**")
                 .permitAll()
+                .and()
+                .csrf().ignoringAntMatchers("/h2-console/**").disable()
+                .headers().frameOptions().disable()
                 .and()
                 .httpBasic();
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("user1")
-                .password("{noop}password1")
-                .authorities("ROLE_USER")
-                .and()
-                .withUser("user2")
-                .password("{noop}password2")
-                .authorities("ROLE_USER");
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery("select username, password, enabled from users where username = ?")
+                .authoritiesByUsernameQuery("select username, authority from authorities where username = ?")
+                .passwordEncoder(new NoEncodingPasswordEncoder());
     }
 }
