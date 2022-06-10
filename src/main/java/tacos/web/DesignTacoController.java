17c80870875f89a8fbafe4f5c186521d7abae99b
hkj <hkj1984@gmail.com>
2022-06-11 00:42:21 +0900

    타코 디자인 API 요청을 처리하는 REST 사용 컨트롤러

--- src/main/java/tacos/web/DesignTacoController.java
+++ src/main/java/tacos/web/DesignTacoController.java
@@ -2,94 +2,57 @@ package tacos.web;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
-import tacos.Ingredient;
 import tacos.Order;
 import tacos.Taco;
-import tacos.User;
-import tacos.data.IngredientRepository;
 import tacos.data.TacoRepository;
-import tacos.data.UserRepository;
 
 import javax.validation.Valid;
-import java.security.Principal;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Optional;
 
 /**
  * 식자재 요청 처리
  */
 @Slf4j
-@Controller
-@RequestMapping("/design")
-@SessionAttributes("order")
+@RestController
+@RequestMapping(value = "/design", produces = {"application/json", "text/xml"})
+@CrossOrigin(origins = "*")
 public class DesignTacoController {
-
-	private final IngredientRepository ingredientRepository;
 
 	private TacoRepository tacoRepository;
 
-	private UserRepository userRepository;
+//	@Autowired
+//	EntityLinks entityLinks;
 
 	@Autowired
-	public DesignTacoController(IngredientRepository ingredientRepository, TacoRepository tacoRepository, UserRepository userRepository) {
-		this.ingredientRepository = ingredientRepository;
+	public DesignTacoController(TacoRepository tacoRepository) {
 		this.tacoRepository = tacoRepository;
-		this.userRepository = userRepository;
 	}
 
-	private List<Ingredient> filterByType(List<Ingredient> ingredientList, Ingredient.Type type) {
-		return ingredientList
-				.stream()
-				.filter(x -> x.getType().equals(type))
-				.collect(Collectors.toList());
+	@GetMapping("/recent")
+	public Iterable<Taco> recentTacos() {
+		PageRequest pageRequest = PageRequest.of(0, 12, Sort.by("createdAt").descending());
+		return tacoRepository.findAll(pageRequest).getContent();
 	}
 
-	@ModelAttribute(name = "order")
-	public Order order() {
-		return new Order();
-	}
-
-	@ModelAttribute(name = "taco")
-	public Taco taco() {
-		return new Taco();
-	}
-
-	@GetMapping
-	public String showDesignForm(Model model, Principal principal) {
-
-		// 식자재 내역 생성
-		List<Ingredient> ingredientList = new ArrayList<>();
-		ingredientRepository.findAll().forEach(i -> ingredientList.add(i));
-
-		Ingredient.Type[] types = Ingredient.Type.values();
-
-		for(Ingredient.Type type : types) {
-			model.addAttribute(type.toString().toLowerCase(), filterByType(ingredientList, type));
+	@GetMapping("/{id}")
+	public ResponseEntity<Taco> tacoById(@PathVariable("id") Long id) {
+		Optional<Taco> optTaco = tacoRepository.findById(id);
+		if(optTaco.isPresent()) {
+			return new ResponseEntity<>(optTaco.get(), HttpStatus.OK);
 		}
-
-//		model.addAttribute("taco", new Taco());
-
-		String username = principal.getName();
-		User user = userRepository.findByUsername(username);
-		model.addAttribute("user", user);
-
-		return "design";
+		return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
 	}
 
-	@PostMapping
-	public String processDesign(@Valid Taco design, Errors errors, @ModelAttribute Order order) {
-		if(errors.hasErrors()) {
-			return "design";
-		}
-
-		Taco saved = tacoRepository.save(design);
-		order.addDesign(saved);
-
-		return "redirect:/orders/current";
+	@PostMapping(consumes="application/json")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Taco postTaco(@RequestBody Taco taco) {
+		return tacoRepository.save(taco);
 	}
+
 }
